0f2dcb42d0e9eb77e2c043f99d54e89068bacb38
Kristaps Ritins <kritinsdev@gmail.com>
2022-05-24 21:46:51 +0300

    starting auth

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 // All listings
 Route::get('/', [ItemController::class, 'index']);
-
 //Show new item form
 Route::get('/item/create', [ItemController::class, 'create']);
-
 // Store new item data
 Route::post('/item', [ItemController::class, 'store']);
-
 //Single listing
 Route::get('/item/{item}', [ItemController::class, 'show']);
-
 //Edit item form
 Route::get('item/{item}/edit', [ItemController::class, 'edit']);
 //Update item
